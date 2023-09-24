bdf8a6ca7c46b85399f1819a7eee2a76104006e8
Main <emanuelcalvopizarro0123@gmail.com>
2023-09-23 20:48:43 -0600

    Commit de 23 de septiembre de 2023 a las 20:48 Pagina 75

---
+++ src/C2_IntroduccionAplicacionesJava/PrimerPrograma.java
@@ -0,0 +1,12 @@
+package C2_IntroduccionAplicacionesJava;
+
+public class PrimerPrograma {// Aquí va el nombre de la clase en cuestión
+    //
+
+    public static void main(String[] args) {//Esta es la declaración del método principal para ejecutar el programa
+        System.out.println("Buenas noches, Java");//Asi es como se imprime un texto por consola en Java. Está se ubica dentro del paquete System.out y creo (Repito: CREO) que se trata de un método
+        /**
+         * Mi primer JavaDoc
+         */
+    }// Se cierra el método Main
+}// Se finaliza el programa
